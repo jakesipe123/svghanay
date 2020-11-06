416edb9debd5f03c21f15442338410761fd49865
jakesipe123 <60919213+jakesipe123@users.noreply.github.com>
2020-11-06 15:14:14 +0800

    Add files via upload

---
+++ index8.php
@@ -0,0 +1,120 @@
+<php>
+<head>
+	<title>SVG</title>
+	<link rel = "stylesheet" href = "styles8.css"	>
+</head>
+<body>
+        <svg class="LineCircle">
+            <circle cx="100" cy="150" r="60" fill="yellow" stroke="yellow" stroke-width="3" />
+			<line x1="40" y1="150" x2="160" y2="150" stroke="black" stroke-width="5"/>
+            <circle cx = "100" cy = "125" r="15" fill = "black" stroke-width="1" stroke = "black"/>
+            <circle cx = "100" cy = "180" r="15" fill = "black" stroke-width="1" stroke = "black"/>
+        </svg>
+        <svg class = "RectCircle">
+            <rect x="30" y="100" width="150px" height="100px" rx="3" ry="3" fill="white" />
+            <circle cx = "65" cy = "150" r="20" fill = "black" stroke-width="1" stroke= "black"/>
+            <circle cx = "147" cy = "150" r="20" fill = "black" stroke-width="1" stroke= "black"/>
+			
+        </svg>    
+            <svg class = "PolylineCircle">
+			    <line x1="0" y1="300" x2="0" y2="0" stroke-width="100" stroke="#04F89A"/>
+				<line x1="100" y1="300" x2="100" y2="0" stroke-width="50" stroke="#15C5EE"/>
+				<line x1="175" y1="300" x2="175" y2="0" stroke-width="50" stroke="#CCF804"/>
+				<circle cx="137" cy="0" r="10" fill="red" stroke-width="1" stroke=""/>
+				<circle cx="62" cy="30" r="10" fill="orange" stroke-width="1" stroke=""/>
+				<circle cx="137" cy="60" r="10" fill="yellow" stroke-width="1" stroke=""/>
+				<circle cx="62" cy="90" r="10" fill="green" stroke-width="1" stroke=""/>
+				<circle cx="137" cy="120" r="10" fill="blue" stroke-width="1" stroke=""/>
+				<circle cx="62" cy="150" r="10" fill="#3504F8" stroke-width="1" stroke=""/>
+				<circle cx="137" cy="180" r="10" fill="violet" stroke-width="1" stroke=""/>
+				<circle cx="62" cy="210" r="10" fill="black" stroke-width="1" stroke=""/>
+				<circle cx="137" cy="240" r="10" fill="skyblue" stroke-width="1" stroke=""/>
+				<circle cx="62" cy="270" r="10" fill="orangered" stroke-width="1" stroke=""/>
+				<circle cx="137" cy="300" r="10" fill="yellowgreen" stroke-width="1" stroke=""/>
+            </svg>
+            <svg class= "PolygonEllipse">
+                <polygon points="95 300, 8 200, 190 200," fill="brown" stroke="brown"/>
+                <polygon points="95 80, 8 200, 190 200," fill="brown" stroke="brown"/>
+                
+				<ellipse cx = "100" cy = "200" rx = "100" ry = "30" fill = "blue" stroke-width="1" stroke= "blue"/>
+            </svg>
+    
+        <svg class="Circlelinerectangle">
+          <circle cx="165" cy="55" r="60" fill="#E5E5E5"/>
+				<rect x="" y="210" width="200" height="90" fill="#1F1A46" transform="skewX(30) skewY(0)"/>
+				<rect x="" y="210" width="200" height="80" fill="#1F1A46" transform="skewX(-35) skewY(0)"/>
+				<rect x="20" y="90" width="40" height="120"  stroke-width="1" stroke="white"fill="black"/>
+				<rect x="40" y="130" width="50" height="5" fill="black"/>
+				<rect x="40" y="110" width="50" height="5" fill="black"/>
+				<rect x="80" y="90" width="40" height="120"  stroke-width="1" stroke="white"fill="black"/>
+				<rect x="" y="110" width="50" height="100" stroke-width="1" stroke="white" fill="black"/>
+				<rect x="52" y="170" width="50" height="40" stroke-width="1" stroke="white" fill="black"/>
+				<rect x="104" y="140" width="50" height="70" stroke-width="1" stroke="white" fill="black"/>
+				<rect x="156" y="125" width="50" height="85" stroke-width="1" stroke="white" fill="black"/>
+				<rect x="10" y="130" width="10" height="10" fill="yellow"/>
+				<rect x="30" y="130" width="10" height="10" fill="yellow"/>
+				<rect x="10" y="160" width="10" height="10" fill="yellow"/>
+				<rect x="30" y="160" width="10" height="10" fill="yellow"/>
+				<rect x="60" y="180" width="10" height="10" fill="yellow"/>
+				<rect x="80" y="180" width="10" height="10" fill="yellow"/>
+				<rect x="115" y="150" width="10" height="10" fill="yellow"/>
+				<rect x="135" y="150" width="10" height="10" fill="yellow"/>
+				<rect x="115" y="180" width="10" height="10" fill="yellow"/>
+				<rect x="135" y="180" width="10" height="10" fill="yellow"/>
+				<rect x="165" y="145" width="10" height="10" fill="yellow"/>
+				<rect x="185" y="145" width="10" height="10" fill="yellow"/>
+				<rect x="165" y="165" width="10" height="10" fill="yellow"/>
+				<rect x="185" y="165" width="10" height="10" fill="yellow"/>
+				<circle cx="115" cy="260" r="50" fill="#E5E5E5" transform="skewX(20)"/>
+         </svg>
+		 
+         <svg class="PolylinePolygon">
+                   <line x1="0" y1="0" x2="500" y2="32" stroke-width="25" stroke="#F763B5"/>
+	      <line x1="-10" y1="30" x2="700" y2="32" stroke-width="25" stroke="orange"/>
+	      <line x1="0" y1="50" x2="400" y2="70" stroke-width="25" stroke="blue"/>
+	      <line x1="-10" y1="82" x2="700" y2="32" stroke-width="25" stroke="green"/>
+	      <line x1="-100" y1="90" x2="200" y2="100" stroke-width="25" stroke="red"/>
+	      <line x1="0" y1="100" x2="0" y2="400" stroke-width="200" stroke="yellow"/>
+	      <line x1="100" y1="200" x2="200" y2="200" stroke-width="200" stroke="skyblue"/>
+
+         </svg>
+                <svg class="CircleRectanglePolylinePolygonEllipse">
+                    <circle cx="90" 
+        cy="85" 
+        r="30" 
+        fill="blue"
+        stroke="#e09b1e"
+        stroke-width="3" />
+        
+<circle cx="150" 
+        cy="220" 
+        r="30"
+        fill="blue" 
+        stroke="#e09b1e"
+        stroke-width="3"/>
+        
+<circle cx="116" 
+        cy="156"
+        r="10" 
+        fill="blue"
+        stroke="#e09b1e"
+        stroke-width="3" />
+<line  x1="34" 
+       y1="44"
+       x2="146" 
+       y2="126" 
+       stroke-width="3" 
+       stroke="yellow"/>
+       
+<line  x1="90" 
+       y1="256"
+       x2="205" 
+       y2="185" 
+       stroke-width="3" 
+       stroke="yellow"/>
+       
+        
+
+                </svg>
+</body>
+</php>
